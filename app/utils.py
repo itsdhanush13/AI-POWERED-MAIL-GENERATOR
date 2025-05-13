6440e2d8c9ab15a724cbe2356c3248380b79c74b
DHANUSH S <itsdhanush966@gmail.com>
2025-05-13 12:14:55 +0530

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,22 +1,15 @@
 import re
 
 def clean_text(text):
-    # Remove HTML tags
     text = re.sub(r'<[^>]*?>', '', text)
-    # Remove URLs
     text = re.sub(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', '', text)
-    # Remove special characters
     text = re.sub(r'[^a-zA-Z0-9 ]', '', text)
-    # Replace multiple spaces with a single space
     text = re.sub(r'\s{2,}', ' ', text)
-    # Trim leading and trailing whitespace
     text = text.strip()
-    # Remove extra whitespace
     text = ' '.join(text.split())
     return text
 
 def extract_email(text):
-    # Simple regex to find emails in the content
     email_pattern = r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
     emails = re.findall(email_pattern, text)
     return emails[0] if emails else None
